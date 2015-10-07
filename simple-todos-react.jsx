2bd21b67a68dbdb947350db81b6e7cf1826f5669
Daijiro Wachi <daijiro.wachi@gmail.com>
2015-10-07 18:23:59 +0900

    tweaks to app file

--- simple-todos-react.jsx
+++ simple-todos-react.jsx
@@ -1,7 +1,10 @@
+// Define a collection to hold our tasks
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   // This code is executed on the client only
 
-  Meteor.startup(function () {
+  Meteor.startup(() => {
     // Use Meteor.startup to render the component after the page is ready
     React.render(<App />, document.getElementById("render-target"));
   });
